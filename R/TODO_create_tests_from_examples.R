ef1755f35b74174af0a41b1f46d1c169765a8e3b
robchallen <rob@terminological.co.uk>
2025-09-19 22:35:25 +0100

    v0.1.0 update

--- R/TODO_create_tests_from_examples.R
+++
@@ -1,4 +0,0 @@
-# TODO: parse file using roxygen2::parse_package()
-# find @example tags in code, create a test_<fn_name> test case file
-# write skeleton and example into file.
-# check for test case files with examples in them. 
